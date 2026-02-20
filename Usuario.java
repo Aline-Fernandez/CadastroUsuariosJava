3ebed9d2ed5979149efa9dd2cf72ed5addd84f10
Aline Fernandes <fernandes.dev@outlook.com>
2026-02-20 12:48:01 -0300

    adicionado get e set para API

--- Usuario.java
+++ Usuario.java
@@ -2,13 +2,15 @@ public class Usuario {
     private String nome;
     private String email;
 
+    public Usuario(){}
+
     public Usuario(String nome, String email) {
         this.nome = nome;
         this.email = email;
     }
 
-    @Override
-    public String toString() {
-        return "Nome: " + nome + " | Email: " + email;
+    public String getNome() {return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getEmail() {return email; }
+    public void setEmail() { this.email = email; }
     }
-}
